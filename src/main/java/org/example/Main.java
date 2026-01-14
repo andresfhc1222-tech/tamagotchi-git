a22c94e2dd1a86d8f619e4bb2e4755917e07a416
andresfhc1222-tech <andresfhc1222@gmail.com>
2026-01-14 22:55:26 +0100

    Implementado menú principal

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hola git");
+import java.util.Scanner;
 
+public class Main {
+
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int opcion = menuPrincipal();
+    }
+
+
+    private static int menuPrincipal() {
+        int opcion;
+        do {
+            System.out.println("""
+                    1. Estado
+                    2. Comer
+                    3. Jugar
+                    4. Dormir
+                    ----------------
+                    0. Salir
+                    Seleccione una opción del menú:
+                    """);
+            opcion = sc.nextInt();
+        } while (opcion < 0 || opcion > 4);
+
+        return opcion;
     }
 }
